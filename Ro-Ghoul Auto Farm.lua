ce8f5b189e102491e377eabbe2a3d79b580949c7
z4gs <54650179+z4gs@users.noreply.github.com>
2022-03-13 11:54:25 -0300

    auto rep and cashout fix
    now it detects server delay when getting reputation quest to prevent kicks

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/78cfeaac5bf992c74c9b0710748cd779/raw/e6e37bfd44e1505f7eeaa8e0695b3d2546693885/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/78cfeaac5bf992c74c9b0710748cd779/raw/9d6f311a99c78b004ded881f23c14862247c0732/Ro-Ghoul"))()

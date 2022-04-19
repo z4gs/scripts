164adaff2351329aa71236961e0401f2730873de
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-18 23:09:02 -0300

    security ++

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/a180f70a65898c939688f74744ae864b/raw/29ac5be67ee8c22aaf9946899a2b51e0c6383997/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/77918f97dc7525a6448e65955d61a268/raw/e51d226618a9ed2bb8b1936339b2aa170c7cdf72/Ro-Ghoul"))()

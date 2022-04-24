9231e0f1c1b7fc8497dfba9e73daf4609774ae3e
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-24 16:09:47 -0300

    Gyakusatsu boss added to farm options section

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/77918f97dc7525a6448e65955d61a268/raw/e51d226618a9ed2bb8b1936339b2aa170c7cdf72/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/fca3518a7b65c06a1c828381beb1bf87/raw/d1b7d67f417c768c9ff28879bc3dbc74477bbef0/Ro-Ghoul"))()

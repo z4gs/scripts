ae99ab894de1ffa8670efef1968db79ec42daf0d
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-25 00:33:22 -0300

    Fixed the game crashing sometimes

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/642cccd375a15bd203f356836e75b1e5078b35c4/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/bf0b8493a9dfc50b90a2cdd6fca53b617e57150b/Ro-Ghoul"))()

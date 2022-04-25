dc38b06c2271a235df474dd65a85ca5f8b8410e5
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-24 21:46:24 -0300

    Fixed Gyakusatsu farm
    - added slider to change distance from bosses when farming
    - fixed stucking after killing Gyakusatsu

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/25d072fbc8b584475847e1be83cc43db/raw/9b52a0268fe927147698f32a07aa6322a3b552c0/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/642cccd375a15bd203f356836e75b1e5078b35c4/Ro-Ghoul"))()

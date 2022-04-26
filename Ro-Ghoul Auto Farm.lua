e0825b6e1678fa8afe49f78b722c3d6dddc349a1
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-26 00:28:28 -0300

    script settings now saves

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/2f1b12b2ce49965d38f8690c923dc585732b24d7/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/178ac1c1a42cef2cabb25bc288251fa440503ae1/Ro-Ghoul"))()

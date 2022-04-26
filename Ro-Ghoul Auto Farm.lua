593388651a828078a7e5b0ec3ab0cb74613d9b63
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-26 07:32:29 -0300

    fix pcall

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/178ac1c1a42cef2cabb25bc288251fa440503ae1/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/a0900efe06e95723951943d68e6c5327ae0615f9/Ro-Ghoul"))()

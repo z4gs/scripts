a24beefb93fe1225556c0311ef18966b59b9a9cb
z4gs <54650179+z4gs@users.noreply.github.com>
2022-04-30 12:35:03 -0300

    obfuscator performance update

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/a0900efe06e95723951943d68e6c5327ae0615f9/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/d355c6ef7d8ce270fd8450e49cf6743d8905f016/Ro-Ghoul"))()

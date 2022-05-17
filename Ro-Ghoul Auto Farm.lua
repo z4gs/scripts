5797b8910bc8ac37eab9def4b57c417a85c7466c
z4gs <54650179+z4gs@users.noreply.github.com>
2022-05-17 14:51:31 -0300

    reputation farm and current quest label fixed

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/6897fa52271f2d48fccaec0fd299c4ed0dabc0fb/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/6bacded3da2e16dc8df491e97b4388a87fcb5c1a/Ro-Ghoul"))()

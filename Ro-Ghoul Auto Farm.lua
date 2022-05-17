763222dd07fccafea813afaaa8b1b8394f1a9f4a
z4gs <54650179+z4gs@users.noreply.github.com>
2022-05-17 00:24:55 -0300

    added auto kick whitelist

--- Ro-Ghoul Auto Farm.lua
+++ Ro-Ghoul Auto Farm.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/65601b0895aa2aea2b93dc0c9c39a80ec70c16ab/Ro-Ghoul"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/z4gs/5b71b8d430eb3582f895dfcfafc1fbfc/raw/6897fa52271f2d48fccaec0fd299c4ed0dabc0fb/Ro-Ghoul"))()
